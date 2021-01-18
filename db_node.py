f97e7aa65b6f7dbc448289aea2583c9cef33660a
Zeng-Lecheng <50188852+Zeng-Lecheng@users.noreply.github.com>
2021-01-18 11:39:46 +0800

    [Refactor](db_node) Rewrite parsing kwargs with dict.get()

--- db_node.py
+++ db_node.py
@@ -1,19 +1,19 @@
 class Author:
     def __init__(self, **kwargs):
-        if 'name' in kwargs:
-            self.name: str = kwargs['name']
-        if 'redirect_target' in kwargs:
-            self.redirect_target: str = kwargs['redirect_target']
+        self.name: str = kwargs['name']
+        self.redirect_target: str = kwargs.get('redirect_target')
+        if self.redirect_target is not None:
             return
-        if 'title' in kwargs:
-            self.title: list[str] = kwargs['title']
-        if 'institution' in kwargs:
-            self.institution: str = kwargs['institution']
+        self.title: list[str] = kwargs.get('title', [])
+        self.institution: str = kwargs.get('institution', '')
+        self.ref_link: str = kwargs.get('ref_link', '')
 
 
 class Title:
     def __init__(self, **kwargs):
-        if 'name' in kwargs:
-            self.name: str = kwargs['name']
-        if 'sub_title' in kwargs:
-            self.sub_title: list[str] = kwargs['sub_title']
+        self.name: str = kwargs['name']
+        self.redirect_target: str = kwargs.get('redirect_target')
+        if self.redirect_target is not None:
+            return
+        self.sub_title: list[str] = kwargs.get('sub_title', [])
+        self.author: list[str] = kwargs.get('author', [])
