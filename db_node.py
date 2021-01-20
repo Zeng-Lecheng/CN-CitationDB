a4e9eef9c4ff4178d3dffb9465091a88f6519277
Zeng-Lecheng <50188852+Zeng-Lecheng@users.noreply.github.com>
2021-01-20 10:47:51 +0800

    [Add](db_node) Add Article

--- db_node.py
+++ db_node.py
@@ -13,3 +13,13 @@ class Title:
         self.redirect_target: str = kwargs.get('redirect_target', '')
         self.sub_title: list[str] = kwargs.get('sub_title', [])
         self.author: list[str] = kwargs.get('author', [])
+
+
+class Article:
+    def __init__(self, **kwargs):
+        self.name: str = kwargs['name']
+        self.author: list[str] = kwargs.get('author', [])
+        self.publisher: str = kwargs.get('publisher', '')
+        self.year: int = kwargs.get('year', 0)
+        self.ref_link: str = kwargs.get('ref_link', '')
+        self.screenshot: str = kwargs.get('screenshot', '')
