d52c575b4580b94a00feb01e33784129bb0fd5c4
Zeng-Lecheng <50188852+Zeng-Lecheng@users.noreply.github.com>
2021-03-03 13:46:19 +0800

    [Refactor] Rewrite using neo4j

--- citationdb.py
+++ citationdb.py
@@ -2,43 +2,67 @@ import json
 import re
 import logging
 
+import neo4j
+
 from pypinyin import lazy_pinyin
 
-import db_node
 
+class CitationDB:
+    def __init__(self, config='config.json'):
+        self.config: dict = json.load(open(config, 'r', encoding='utf-8'))
+        self.driver = neo4j.GraphDatabase.driver(self.config['neo4j_uri'],
+                                                 auth=(self.config['neo4j_user'],
+                                                       self.config['neo4j_password']))
 
-logging.basicConfig(level=logging.DEBUG)
+    def close(self):
+        # Don't forget to close the driver connection when you are finished with it
+        self.driver.close()
 
+    def write_query(self, query_text):
+        def _write_query(tx):
+            result = tx.run(query_text)
+            return list(result)
 
-class Db:
-    def __init__(self, filename: str):
-        self.data: dict = json.load(open(filename, 'r', encoding='utf-8'))
-        self.filename = filename
+        with self.driver.session() as session:
+            res = session.write_transaction(_write_query)
+            for item in res:
+                print(item)
+            return res
+
+    def read_query(self, query_text):
+        def _write_query(tx):
+            result = tx.run(query_text)
+            return list(result)
+
+        with self.driver.session() as session:
+            res = session.read_transaction(_write_query)
+            for item in res:
+                print(item)
+            return res
 
     def add_author(self, **kwargs):
-        """
-        :param kwargs:
-            name: str. Name of the author. Also used as the key for indexing this author. Should be fullname or the most
-                       commonly used name if it is not a redirection.
-            redirect_target: str. Name other than the fullname or mostly commonly used name. Searching process will be
-                                  redirected to its target.
-            title: list[str]. A list of titles of this author.
-            institution: str. The institution this author belongs to.
-        """
-        if kwargs['name'] in self.data['author']:
+        name = kwargs.get('name', '')
+        if not name:
+            logging.error(f'[add_author()]: No name provided.')
+            return
+        ref_link = kwargs.get('ref_link', '')
+        institution = kwargs.get('institution', '')
+        title = kwargs.get('title', [])
+        dup_check = self.read_query(f'MATCH (a:Author) WHERE a.name="{kwargs["name"]}" RETURN a')
+        if len(dup_check) != 0:
             logging.error(f'[add_author()]: {kwargs["name"]} already exists')
             return
-        author = db_node.Author(**kwargs)
-        if author.redirect_target:
-            logging.error(f'[add_author()]: You are adding {author.name} as a redirect. '
+        if kwargs.get('alias_of'):
+            logging.error(f'[add_author()]: You are adding {kwargs.get("name")} as a redirect. '
                           f'Use add_author_redirect() instead')
-        self.data['author'][kwargs['name']] = author.__dict__
+            return
+        self.write_query(f'CREATE (a:Author {{name="{name}", ref_link="{ref_link}", institution="{institution}"}})')
 
         # Possible alias
-        name_split = re.split(r'-|\s+', author.name)
+        name_split = re.split(r'-|\s+', name)
         alias = []
-        if re.search(r'[\u4e00-\u9fa5]', author.name):  # Chinese characters
-            pinyin_list = lazy_pinyin(author.name)
+        if re.search(r'[\u4e00-\u9fa5]', name):  # Chinese characters
+            pinyin_list = lazy_pinyin(name)
             given_1 = ''.join(pinyin_list[1:])
             given_2 = '-'.join([f'{s[0].upper()}{s[1:]}' for s in pinyin_list[1:]])
             family = pinyin_list[0]
@@ -49,7 +73,7 @@ class Db:
             alias.append(f'{given_1[0].upper()} {family[0].upper()}{family[1:]}')
             alias.append(f'{given_1[0].upper()}. {family[0].upper()}{family[1:]}')
         else:
-            alias = [author.name.lower(), author.name.upper(),
+            alias = [name.lower(), name.upper(),
                      '.'.join([s[0] for s in name_split[:-1]]) + '. ' + name_split[-1],
                      '. '.join([s[0] for s in name_split[:-1]]) + '. ' + name_split[-1],
                      ' '.join([s[0] for s in name_split[:-1]]) + ' ' + name_split[-1],
@@ -58,120 +82,37 @@ class Db:
 
         alias = list(set(alias))
         for item in alias:
-            if item == author.name:
+            if item == name:
                 continue
-            self.add_author_redirect(item, author.name)
+            self.add_author_redirect(item, name)
 
-        # Check Titles
-        for item in author.title:
-            if item not in self.data['title']:
-                self.add_title(name=item)
-            title = db_node.Title(**self.data['title'][item])
-            while title.redirect_target:
-                title = db_node.Title(**self.data['title'][title.redirect_target])
-            title.author.append(author.name)
-
-            self.data['title'][title.name] = title.__dict__
+            # Check Titles
+        for item in title:
+            self.add_title(item)    # Dup check is in add_title()
+            self.write_query(f'MATCH (t:Title) WHERE t.name="{item}"'
+                             f'MATCH (a:Author) WHERE a.name="{name}"'
+                             f'CREATE (a)-[:Has]->(t)')
 
     def add_author_redirect(self, name: str, target: str):
-        redirect = db_node.Author(name=name, redirect_target=target)
-        if target not in self.data['author']:
+        dup_check = self.read_query(f'MATCH (a:Author) WHERE a.name="{name}" RETURN a')
+        if len(dup_check) != 0:
+            logging.error(f'[add_author_redirect()]: {name} already exists.')
+            return
+        target_existence_check = self.read_query(f'MATCH (a:Author) WHERE a.name="{target}" RETURN a')
+        if len(target_existence_check) == 0:
             logging.error(f'[add_author_redirect()]: Target {target} does not exist.')
             return
+        self.write_query(f'MATCH (tar:Author) WHERE tar.name="{target}"'
+                         f'CREATE (a:Author {{name="{name}"}})'
+                         f'CREATE (a)-[:Alias_of]->(tar)')
 
-        target_author = db_node.Author(**self.data['author'][target])
-        while target_author.redirect_target:
-            target_author = db_node.Author(**self.data['author'][target_author.redirect_target])
-        redirect.redirect_target = target_author.name
-
-        if name in self.data['author']:
-            logging.warning(f'[add_author_redirect()]: {name} already exists.')
-            old_author = db_node.Author(**self.data['author'][name])
-
-            target_author.title.extend(old_author.title)
-
-        self.data['author'][name] = redirect.__dict__
-
-    def edit_author(self, name, **kwargs):
-        if name not in self.data['author']:
-            logging.error(f'[edit_author()]: Author {name} does not exist. Use add_author() instead.')
-            return
-        if kwargs.get('redirect_target', ''):
-            logging.error(f'[edit_author()]: Set redirect with add_redirect() instead.')
-            return
-        while self.data['author'][name]['redirect_target']:
-            name = self.data['author'][name]['redirect_target']
-        old_author = db_node.Author(**self.data['author'][name])
-        new_author = db_node.Author(**self.data['author'][name])
-        for key in kwargs:
-            exec(f'new_author.{key} = kwargs["{key}"]')
-        removed_title = set(old_author.title) - set(new_author.title)
-        added_title = set(new_author.title) - set(old_author.title)
-        for title_name in removed_title:
-            title = db_node.Title(**self.data['title'][title_name])
-            title.author.remove(old_author.name)
-            self.data['title'][title_name] = title.__dict__
-        for title_name in added_title:
-            title = db_node.Title(**self.data['title'].get(title_name, {'name': title_name}))
-            title.author.append(old_author.name)
-            self.data['title'][title_name] = title.__dict__
-        self.data['author'][new_author.name] = new_author.__dict__
-
-    def remove_author(self, name: str):
-        if name not in self.data['author']:
-            logging.error(f'[remove_author()]: {name} does not exist.')
-            return
-        author = db_node.Author(**self.data['author'][name])
-        for title_name in author.title:
-            title = db_node.Title(**self.data['title'][title_name])
-            title.author.remove(name)
-            self.data['title'][title_name] = title.__dict__
-        del self.data['author'][name]
-
-    def add_title(self, **kwargs):
-        if kwargs['name'] in self.data['title']:
-            logging.error(f'[add_title()]: {kwargs["name"]} already exists.')
-            return
-        title = db_node.Title(**kwargs)
-
-        for title_name in title.sub_title:
-            if title_name not in self.data['title']:
-                self.add_title(name=title_name)
-        self.data['title'][kwargs['name']] = title.__dict__
-
-    def add_title_redirect(self, name: str, target: str):
-        redirect = db_node.Title(name=name, redirect_target=target)
-        if target not in self.data['title']:
-            logging.error(f'[add_title_redirect()]: Target {target} does not exist.')
+    def add_title(self, name: str):
+        dup_check = self.read_query(f'MATCH (t:Title) WHERE t.name="{name}" RETURN t')
+        if len(dup_check) == 0:
+            logging.error(f'[add_title()]: Title {name} already exists.')
             return
 
-        target_title = db_node.Title(**self.data['title'][target])
-        while target_title.redirect_target:
-            target_title = db_node.Title(**self.data['title'][target_title.redirect_target])
-        redirect.redirect_target = target_title.name
+        self.write_query(f'CREATE (t:Title{{name: "{name}"}})')
 
-        if name in self.data['title']:
-            logging.warning(f'[add_title_redirect()]: {name} already exists.')
-            old_title = db_node.Title(**self.data['title'][name])
-
-            target_title.sub_title.extend(old_title.sub_title)
-            target_title.author.extend(old_title.author)
-        self.data['title'][name] = redirect.__dict__
-
-    def write_db(self):
-        json.dump(self.data, open(self.filename, 'w', encoding='utf-8'), indent=2, ensure_ascii=False)
-
-    def clean_up(self):
-        for name in list(self.data['author'].keys()):
-            data = self.data['author'][name]
-            author = db_node.Author(**data)
-            if author.redirect_target and author.redirect_target not in self.data['author']:
-                del self.data['author'][name]
-        for name in list(self.data['title'].keys()):
-            data = self.data['title'][name]
-            title = db_node.Title(**data)
-            if title.redirect_target and title.redirect_target not in self.data['title']:
-                del self.data['title'][name]
-            for sub_title_name in title.sub_title:
-                if sub_title_name not in self.data['title']:
-                    title.sub_title.remove(sub_title_name)
+    def check_dup_author(self, name: str):
+        return bool(self.read_query(f'MATCH (a:Author) WHERE a.name="{name}" RETURN a'))
